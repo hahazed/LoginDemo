340448b68a0ebcd4feab92c1d4097161cc5ad481
hahazed <39752738+hahazed@users.noreply.github.com>
2018-05-30 12:47:30 +0800

    first commit

---
+++ Check.java
@@ -0,0 +1,12 @@
+package org.oyj.operate;
+
+public class Check {
+      public boolean validate(String name,String password) {
+    	  if(name.equals("lixinghua")
+    			  &&password.equals("mldn")) {
+    		  return true;
+    	  }else {
+    		  return false;
+    	  }
+      }
+}  
---
+++ LoginDemo01.java
@@ -0,0 +1,8 @@
+package org.oyj.operate;
+
+public class LoginDemo01 {
+    public static void main(String args[]) {
+    	Operate operate=new Operate(args);
+    	System.out.println(operate.login());
+    }
+}
